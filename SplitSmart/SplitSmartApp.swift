1fc9eddca8891ca7f109f7d7c040d9524b1bb75d
David Mbugua <47davidmbugua23@gmail.com>
2025-03-25 22:49:12 +0300

    Initial Commit

---
+++ SplitSmart/SplitSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitSmartApp.swift
+//  SplitSmart
+//
+//  Created by David Mbugua on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SplitSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SplitSmartTests/SplitSmartTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitSmartTests.swift
+//  SplitSmartTests
+//
+//  Created by David Mbugua on 25/03/2025.
+//
+
+import Testing
+@testable import SplitSmart
+
+struct SplitSmartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
